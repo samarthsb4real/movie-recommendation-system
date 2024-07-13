5112754898faa47e8192cf9f14fefa07f81b47e3
samarthsb4real <samarthbhadane23@gmail.com>
2024-07-13 16:31:08 +0530

    displaying best 6 results

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
 import streamlit as st
 import pickle
 
+#creating the recommend function
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:7]
+    movies_list = sorted(list(enumerate(distances)), key=lambda x: x[1], reverse=True)[1:7]
 
     recommendations_list = []
-
     for i in movies_list:
-        recommendations_list.append(movies[i[0]].title)
+        recommendations_list.append(movies.iloc[i[0]].title)
+
     return recommendations_list
-
-
-
-
-
 
 st.title('Movie Recommendation System')
 
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 movies = pickle.load(open('movies.pkl', 'rb'))
 
-movies = movies['title'].values
 
-selected = st.selectbox('Enter a movie title', (movies))
+selected = st.selectbox('Enter a movie title', movies['title'].values)
 
 st.number_input(label='How many similar movies do you want to recommend?', min_value=1,max_value=10)
 
@@ -32,3 +27,4 @@ if st.button(label='Show Recommendations'):
     recommendations = recommend(selected)
     for i in recommendations:
         st.write(i)
+
